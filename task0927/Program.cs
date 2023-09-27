5b08f57405e18a4b1bd4073832cd3e0cfeb5de83
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-09-28 00:05:19 +0400

    add

---
+++ task0927/Program.cs
@@ -0,0 +1,18 @@
+﻿static bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+        return false;
+
+    while (n > 1)
+    {
+        if (n % 2 != 0)
+            return false;
+
+        n = n / 2;
+    }
+    return true;
+}
+Console.WriteLine("reqem daxil edin");
+int sayi = int.Parse(Console.ReadLine());
+
+  
